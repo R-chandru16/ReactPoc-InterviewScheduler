cc40215ce5b3ee80d2ce8c27f04edc7e0477893f
Jeevithasri17 <jeevithasri.kathiresan@kanini.com>
2022-09-06 18:13:38 +0530

    Add Candidate - with populated values of dropdown(level, job)

--- src/Components/Candidate/Candidate.js
+++ src/Components/Candidate/Candidate.js
@@ -1,282 +1,231 @@
-import React, { Component } from "react";
+import axios from 'axios';
+import React, { useState,useEffect } from "react";
 import Sidebar from '../Sidebar';
-
 import CandidateService from "../../Services/CandidateService";
-
-// import '../css/Create.css';
 import './../Job/Job.css';
-
-
 import 'react-toastify/dist/ReactToastify.css';
 import { Link } from "react-router-dom";
 import JobService from "../../Services/JobService";
+import { FaSortAlphaDownAlt } from 'react-icons/fa';
+// import swal from 'sweetalert';
+
+function AddCandidate(){
+    const Job_Baseurl="https://localhost:44348/api/Jobs";
+    const Candidate_Baseurl="https://localhost:44348/api/Candidate";
 
 
-class Candidate extends Component{
-    constructor(props){
-        super(props)
-        this.state={
-            name:'',
-            dob:'',
-            address:'',
-            mobileno:'',
-            qualification:'',
-            email:'',
-            level_id:0,
-            job_id:0,
-            resume:'',           
-            errors:{}   
-            
+    const [levellist, setLevellist] = useState([]);
+    const [joblist, setJoblist] = useState([]);
+
+    const [candidateInput, setCandidate] = useState({
+        name : '',
+        dob : '',
+        address : '',
+        mobileno : '',
+        qualification : '',
+        email: '',
+        level_id : 0,
+        job_id : 0,
+        resume : '',
+
+
+    });
+
+    // const [errorlist, setError]=useState([]);
+
+    const handleInput = (e) => {
+        e.persist();
+        setCandidate({...candidateInput, [e.target.name]:e.target.value});
+
+    }
+
+    useEffect(()=>{
+        axios.get(Job_Baseurl + `/GetAllInterviewLevels` ).then(res=>{
+           // if(res.data.status===200){
+                console.log(res.data);
+
+                setLevellist(res.data);
+
+           // }
+        })
+    },[]);
+
+    useEffect(()=>{
+        axios.get(Job_Baseurl + `/GetAllJobs` ).then(res=>{
+           // if(res.data.status===200){
+                console.log(res.data);
+
+                setJoblist(res.data);
+
+           // }
+        })
+    },[]);
+        
+       
+
+    // const submitCandidate = (e)=>{
+    //     e.preventDefault();
+
+    //     const formData = new FormData();
+    //     formData.append('name', candidateInput.name);
+    //     formData.append('dob', candidateInput.dob);
+    //     formData.append('address', candidateInput.address);
+    //     formData.append('mobileno', candidateInput.mobileno);
+    //     formData.append('qualification', candidateInput.qualification);
+    //     formData.append('email', candidateInput.email);
+    //     formData.append('level_id', candidateInput.level_id);
+    //     formData.append('job_id', candidateInput.job_id);
+    //     formData.append('resume', candidateInput.resume);
+
+
+    //     axios.post(Candidate_Baseurl + `/AddCandidate`, formData).then(res=>{
+    //         if(res.data.status===200){
+    //             alert("Candidate added Successfully!");
+    //             // swal("Success", res.data.message,"success");
+    //             // setError([]);
+    //         }
+    //         else{
+    //             // swal("All fields are mandatory","","error");
+    //             // setError(res.data.errors);
+    //             alert("Something went wrong!");
+
+    //         }
+    //     }
+    //     )
+    // }
+
+    const submitCandidate = (e) => {
+        e.persist();
+
+        const data = {
+            name: candidateInput.name,
+            dob: candidateInput.dob,
+            address: candidateInput.address,
+            mobileno: candidateInput.mobileno,
+            qualification: candidateInput.qualification,
+            email: candidateInput.email,
+            level_id: parseInt(candidateInput.level_id),
+            job_id: parseInt(candidateInput.job_id),
+            resume: candidateInput.resume,
+
         }
-     
-        this.name=this.name.bind(this);     
-        this.dob=this.dob.bind(this);  
-        this.address=this.address.bind(this);
-        this.mobileno=this.mobileno.bind(this);
-        this.qualification=this.qualification.bind(this);
-        this.email=this.email.bind(this);
-        this.level_id=this.level_id.bind(this);
-        this.job_id=this.job_id.bind(this);
-        this.resume=this.resume.bind(this);
-        this.addCandidate=this.addCandidate.bind(this);
+
+        axios.post(Candidate_Baseurl + `/AddCandidate`, data).then(res => {
+            if(res.data.status === 200){
+                console.log(res);
+                window.location="/viewcandidate";
+
+                alert("Candidate added Successfully!");
+            }
+            else{
+                alert("Something went wrong!");
+            }
+        });
+
+
+    }
+
+    return(
+        <div>
+               <div class="side">
+        <Sidebar />
+    </div>
+        <form className="addformjob" onSubmit={submitCandidate} encType="multipart/form-data">
+            <h2><strong>Add Candidate</strong></h2>
+    
+          {/* <br></br> */}
+            <label >Name</label>
+            <input  id="name" type="text"  name="name" onChange={handleInput} value={candidateInput.name} ></input><br></br>
+            {/* <div className="errorMsgJob">{errorlist.name}</div> */}
+            <br></br>
+
+            <label >DOB</label>
+            <input  id="dob" type="date"  name="dob" onChange={handleInput} value={candidateInput.dob} ></input><br></br>
+            {/* <div className="errorMsgJob">{errorlist.dob}</div> */}
+            <br></br>
+
+            <label >Address</label>
+            <input  id="address" type="text"  name="address" onChange={handleInput} value={candidateInput.address} ></input><br></br>
+            {/* <div className="errorMsgJob">{errorlist.address}</div> */}
+            <br></br>
+
+            <label >Mobile No.</label>
+            <input  id="mobileno" type="text"  name="mobileno" onChange={handleInput} value={candidateInput.mobileno} ></input><br></br>
+            {/* <div className="errorMsgJob">{errorlist.mobileno}</div> */}
+            <br></br>
+
+            <label >Qualification</label>
+            <input  id="qualification" type="text"  name="qualification" onChange={handleInput} value={candidateInput.qualification} ></input><br></br>
+            {/* <div className="errorMsgJob">{errorlist.qualification}</div> */}
+            <br></br>
+
+            <label >Email</label>
+            <input  id="email" type="text"  name="email" onChange={handleInput} value={candidateInput.email} ></input><br></br>
+            {/* <div className="errorMsgJob">{errorlist.email}</div> */}
+            <br></br>
+
+            <label >Level</label>         
+            <select name="level_id" onChange={handleInput} value={candidateInput.level_id}>
+              <option>
+                  Select Level
+              </option>
+              {
+                levellist.map((item)=>{
+                  return(
+                    <option value={item.id} key={item.id}>{item.level}</option>
+                  )
+                })
+              }
+            </select>
+            {/* <div className="errorMsgJob">{errorlist.level_id}</div> */}
+            <br></br>
+
+            <label >Job Role</label>
+            <select name="job_id" onChange={handleInput} value={candidateInput.job_id}>
+              <option>
+                  Select Job Role
+              </option>
+              {
+                joblist.map((item)=>{
+                  return(
+                    <option value={item.id} key={item.id}>{item.jobRole}</option>
+                  )
+                })
+              }
+            </select>
+            {/* <div className="errorMsgJob">{errorlist.job_id}</div> */}
+            <br></br>
+
+            <label >Resume</label>
+            <input  id="resume" type="file"  name="resume" onChange={handleInput} value={candidateInput.resume} ></input><br></br>
+            {/* <div className="errorMsgJob">{errorlist.resume}</div> */}
+
+            <br></br>
+
+            <br></br>
+    {/* </div> */}
+
+            <button id="jobb" className="btn btn-success" type="submit">Create</button>
+            <br></br>
+            <br></br>
+            {/* <Link to={'/addcandidate'}><button className="btn btn-outline-dark" type="submit" >Back</button>
+        </Link>  */}
+            <br></br>
+
+            <Link to={'/addcandidate'}><button className="btn btn-outline-dark" type="submit" onClick={()=>{window.location='/viewcandidate'}}>Back</button>
+            </Link> 
+                <br></br>
+         
+        </form>
+    </div>
+    )
+}
+
          
  
   
-}    
-    addCandidate=(e)=>{
-     
-    
-       e.preventDefault();
-        let errors={};
-       var pattern=new RegExp( /^[0-9a-zA-Z]+$/);
-       var pat=new RegExp(/[a-zA-Z]/);
-        let formvalidstatus=true;  
-         
-        // if(this.state.jobId==""){
-            
-        //  formvalidstatus=false;
-        //  errors["jobId"]="Please enter your jobid !";
-        
-        // }      
-        // else if(!pattern.test(this.state.jobId)){
-            
-        //   formvalidstatus=false;
-        //   errors["jobId"]="Please enter only letters and numbers !";
-         
-        //  }   
-
-        
-        if((this.state.name)==""){
-          
-            formvalidstatus=false;
-            errors["name"]="Please enter your name!";
-        } 
-        if((this.state.dob)==""){
-            
-          formvalidstatus=false;
-          errors["dob"]="Please enter your DOB!";
-         
-        }   
-        if((this.state.address)==""){
-            
-            formvalidstatus=false;
-            errors["address"]="Please enter your Address!";
-           
-          }   
-        
-      if((this.state.mobileno)==""){
-          
-        formvalidstatus=false;
-        errors["mobileno"]="Please enter Mobile Number !";
-        
-    }
-
-    if((this.state.qualification)==""){
-            
-        formvalidstatus=false;
-        errors["qualification"]="Please enter Qualification!";
-       
-      }   
-      if((this.state.email)==""){
-            
-        formvalidstatus=false;
-        errors["email"]="Please enter Email!";
-       
-      }   
-      if((this.state.level_id)==0){
-            
-        formvalidstatus=false;
-        errors["level_id"]="Please enter Level!";
-       
-      }   
-      if((this.state.job_id)==0){
-            
-        formvalidstatus=false;
-        errors["job_id"]="Please enter your Job Role!";
-       
-      }   
-      if((this.state.resume)==""){
-            
-        formvalidstatus=false;
-        errors["resume"]="Please upload Resume!";
-       
-      }   
-
-
-
-      this.setState({
-          errors:errors
-          
-      });
-      if(formvalidstatus==true){
-    
-        let candidate={name:this.state.name,
-                    
-                    dob:this.state.dob,
-                
-                      address:this.state.address,
-                      mobileno:this.state.mobileno,
-                      qualification:this.state.qualification,
-                      email:this.state.email,
-                      level_id:parseInt(this.state.level_id),
-                      job_id:parseInt(this.state.job_id),
-                      resume:this.state.resume,
-
-                    };
-
-                    console.log(this.state.name);
-        
-                    console.log('candidate=>'+candidate.name+ " " + candidate.dob
-                     + " " + candidate.address + " " + candidate.mobileno + " " + candidate.qualification + " " + candidate.email + " " + candidate.level_id + " " + candidate.job_id + " " + candidate.resume);
-                    
-                     console.log('candidate=>'+JSON.stringify(candidate));
-                    
-                    CandidateService.AddCandidate(candidate).then(res=>{
-                        
-                        window.location="/addcandidate";
-                        localStorage.setItem('Candidatestatus',true);
-                       
-                        alert('Candidate Added successfully')
-                    });
-                   
-
-    }
-   }
 
     
-     name=(event)=>{
-        this.setState({name:event.target.value});
-     }
     
-     dob=(event)=>{
-        this.setState({dob:event.target.value});
-     }
-     
-     address=(event)=>{
-        this.setState({address:event.target.value});
-     }
-     mobileno=(event)=>{
-        this.setState({mobileno:event.target.value});
-     }
-     qualification=(event)=>{
-        this.setState({qualification:event.target.value});
-     }
-     email=(event)=>{
-        this.setState({email:event.target.value});
-     }
-     level_id=(event)=>{
-        this.setState({level_id:event.target.value});
-     }
-     job_id=(event)=>{
-        this.setState({job_id:event.target.value});
-     }
-     resume=(event)=>{
-        this.setState({resume:event.target.value});
-     }
-     cancel(){
-        window.location="/";
-     }
+export default AddCandidate;
 
-     render(){
-        return(
-            <div>
-                   <div class="side">
-            <Sidebar />
-        </div>
-            <form className="addformjob">
-                <h2><strong>Add Candidate</strong></h2>
-        
-              {/* <br></br> */}
-                <label >Name</label>
-                <input  id="name" type="text"  name="name" onChange={(e)=>this.name(e)}></input><br></br>
-                <div className="errorMsgJob">{this.state.errors.name}</div>
-
-                <br></br>
-
-                <label >DOB</label>
-                <input  id="dob" type="date"  name="dob" onChange={(e)=>this.dob(e)}></input><br></br>
-                {/* <div className="errorMsgJob">{this.state.errors.dob}</div> */}
-                <br></br>
-                <label >Address</label>
-                <input  id="address" type="text"  name="address" onChange={(e)=>this.address(e)}></input><br></br>
-                <div className="errorMsgJob">{this.state.errors.address}</div>
-
-                <br></br>
-                <label >Mobile No.</label>
-                <input  id="mobileno" type="text"  name="mobileno" onChange={(e)=>this.mobileno(e)}></input><br></br>
-                <div className="errorMsgJob">{this.state.errors.mobileno}</div>
-
-                <br></br>
-                <label >Email</label>
-                <input  id="email" type="text"  name="email" onChange={(e)=>this.email(e)}></input><br></br>
-                <div className="errorMsgJob">{this.state.errors.email}</div>
-
-                <br></br>
-                <label >Level</label>
-                <input  id="level_id" type="number"  name="level_id" onChange={(e)=>this.level_id(e)}></input><br></br>
-                <div className="errorMsgJob">{this.state.errors.level_id}</div>
-
-                <br></br>
-                <label >Job Role</label>
-                <input  id="job_id" type="number"  name="job_id" onChange={(e)=>this.job_id(e)}></input><br></br>
-                <div className="errorMsgJob">{this.state.errors.job_id}</div>
-
-                <br></br>
-                <label >Resume</label>
-                <input  id="resume" type="file"  name="resume" onChange={(e)=>this.resume(e)}></input><br></br>
-                <div className="errorMsgJob">{this.state.errors.resume}</div>
-
-                <br></br>
-
-                <br></br>
-        {/* </div> */}
-
-                <button id="jobb" className="btn btn-success" onClick={this.addCandidate}>Create</button>
-                <br></br>
-                <br></br>
-                <Link to={'/addcandidate'}><button className="btn btn-outline-dark" type="submit" onClick={()=>{window.location='/addcandidate'}}>Back</button>
-            </Link> 
-                <br></br>
-             
-            </form>
-        </div>
-            
-        )
-     }
-}
-export default Candidate
-
-
-// export class Job extends React.Component{
-//   render(){
-//       return(
-//               <div className="col-md-4">
-//                  <form className="addformjob">
-
-//                   <h2>{this.props.jobrole}</h2>
-//                   </form>
-                 
-//               </div>
-//           )
-//   }
-// }
---
+++ src/Components/Candidate/Candidate1.js
@@ -0,0 +1,294 @@
+import React, { Component, useState } from "react";
+import Sidebar from '../Sidebar';
+import CandidateService from "../../Services/CandidateService";
+// import '../css/Create.css';
+import './../Job/Job.css';
+import 'react-toastify/dist/ReactToastify.css';
+import { Link } from "react-router-dom";
+import JobService from "../../Services/JobService";
+
+
+class Candidate extends Component{
+    constructor(props){
+        super(props)
+        this.state={
+            name:'',
+            dob:'',
+            address:'',
+            mobileno:'',
+            qualification:'',
+            email:'',
+            level_id:0,
+            job_id:0,
+            resume:'',           
+            errors:{}   
+            
+        }
+     
+        this.name=this.name.bind(this);     
+        this.dob=this.dob.bind(this);  
+        this.address=this.address.bind(this);
+        this.mobileno=this.mobileno.bind(this);
+        this.qualification=this.qualification.bind(this);
+        this.email=this.email.bind(this);
+        this.level_id=this.level_id.bind(this);
+        this.job_id=this.job_id.bind(this);
+        this.resume=this.resume.bind(this);
+        this.addCandidate=this.addCandidate.bind(this);
+         
+ 
+  
+}    
+    addCandidate=(e)=>{
+     
+    
+       e.preventDefault();
+        let errors={};
+       var pattern=new RegExp( /^[0-9a-zA-Z]+$/);
+       var pat=new RegExp(/[a-zA-Z]/);
+        let formvalidstatus=true;  
+         
+        // if(this.state.jobId==""){
+            
+        //  formvalidstatus=false;
+        //  errors["jobId"]="Please enter your jobid !";
+        
+        // }      
+        // else if(!pattern.test(this.state.jobId)){
+            
+        //   formvalidstatus=false;
+        //   errors["jobId"]="Please enter only letters and numbers !";
+         
+        //  }   
+
+        
+        if((this.state.name)==""){
+          
+            formvalidstatus=false;
+            errors["name"]="Please enter your name!";
+        } 
+        if((this.state.dob)==""){
+            
+          formvalidstatus=false;
+          errors["dob"]="Please enter your DOB!";
+         
+        }   
+        if((this.state.address)==""){
+            
+            formvalidstatus=false;
+            errors["address"]="Please enter your Address!";
+           
+          }   
+        
+      if((this.state.mobileno)==""){
+          
+        formvalidstatus=false;
+        errors["mobileno"]="Please enter Mobile Number !";
+        
+    }
+
+    if((this.state.qualification)==""){
+            
+        formvalidstatus=false;
+        errors["qualification"]="Please enter Qualification!";
+       
+      }   
+      if((this.state.email)==""){
+            
+        formvalidstatus=false;
+        errors["email"]="Please enter Email!";
+       
+      }   
+      if((this.state.level_id)==0){
+            
+        formvalidstatus=false;
+        errors["level_id"]="Please enter Level!";
+       
+      }   
+      if((this.state.job_id)==0){
+            
+        formvalidstatus=false;
+        errors["job_id"]="Please enter your Job Role!";
+       
+      }   
+      if((this.state.resume)==""){
+            
+        formvalidstatus=false;
+        errors["resume"]="Please upload Resume!";
+       
+      }   
+
+
+
+      this.setState({
+          errors:errors
+          
+      });
+      if(formvalidstatus==true){
+    
+        let candidate={name:this.state.name,
+                    
+                    dob:this.state.dob,
+                
+                      address:this.state.address,
+                      mobileno:this.state.mobileno,
+                      qualification:this.state.qualification,
+                      email:this.state.email,
+                      level_id:parseInt(this.state.level_id),
+                      job_id:parseInt(this.state.job_id),
+                      resume:this.state.resume,
+
+                    };
+
+                    let level={
+                      level:this.state.level
+
+                    };
+
+                    // console.log(this.state.name);
+        
+                    console.log('candidate=>'+candidate.name+ " " + candidate.dob
+                     + " " + candidate.address + " " + candidate.mobileno + " " + candidate.qualification + " " + candidate.email + " " + candidate.level_id + " " + candidate.job_id + " " + candidate.resume);
+                    
+                     console.log('candidate=>'+JSON.stringify(candidate));
+                    
+                     let levellist = useState([]);
+                     const [levellist1, setLevellist] = useState([]);
+                     JobService.GetAllInterviewLevels(level).then(res=>{
+                      if(res.data.status===200){
+                        setLevellist(res.data.level);
+                      }
+                     })
+
+                    CandidateService.AddCandidate(candidate).then(res=>{                        
+                        window.location="/addcandidate";
+                        localStorage.setItem('Candidatestatus',true);
+                       
+                        alert('Candidate Added successfully')
+                    });
+                   
+
+    }
+   }
+
+    
+     name=(event)=>{
+        this.setState({name:event.target.value});
+     }
+    
+     dob=(event)=>{
+        this.setState({dob:event.target.value});
+     }
+     
+     address=(event)=>{
+        this.setState({address:event.target.value});
+     }
+     mobileno=(event)=>{
+        this.setState({mobileno:event.target.value});
+     }
+     qualification=(event)=>{
+        this.setState({qualification:event.target.value});
+     }
+     email=(event)=>{
+        this.setState({email:event.target.value});
+     }
+    //  level_id=(event)=>{
+    //     this.setState({level_id:event.target.value});
+    //  }
+
+    level_id=(event)=>{
+      // const [levellist, setLevellist] = useState([]);
+      this.setState({level_id:event.target.value});
+   }
+
+     job_id=(event)=>{
+        this.setState({job_id:event.target.value});
+     }
+     resume=(event)=>{
+        this.setState({resume:event.target.value});
+     }
+     cancel(){
+        window.location="/";
+     }
+
+     render(){
+        return(
+            <div>
+                   <div class="side">
+            <Sidebar />
+        </div>
+            <form className="addformjob">
+                <h2><strong>Add Candidate</strong></h2>
+        
+              {/* <br></br> */}
+                <label >Name</label>
+                <input  id="name" type="text"  name="name" onChange={(e)=>this.name(e)}></input><br></br>
+                <div className="errorMsgJob">{this.state.errors.name}</div>
+
+                <br></br>
+
+                <label >DOB</label>
+                <input  id="dob" type="date"  name="dob" onChange={(e)=>this.dob(e)}></input><br></br>
+                {/* <div className="errorMsgJob">{this.state.errors.dob}</div> */}
+                <br></br>
+                <label >Address</label>
+                <input  id="address" type="text"  name="address" onChange={(e)=>this.address(e)}></input><br></br>
+                <div className="errorMsgJob">{this.state.errors.address}</div>
+
+                <br></br>
+                <label >Mobile No.</label>
+                <input  id="mobileno" type="text"  name="mobileno" onChange={(e)=>this.mobileno(e)}></input><br></br>
+                <div className="errorMsgJob">{this.state.errors.mobileno}</div>
+
+                <br></br>
+                <label >Email</label>
+                <input  id="email" type="text"  name="email" onChange={(e)=>this.email(e)}></input><br></br>
+                <div className="errorMsgJob">{this.state.errors.email}</div>
+
+                <br></br>
+                <label >Level</label>
+                {/* <input  id="level_id" type="number"  name="level_id" onChange={(e)=>this.level_id(e)}></input><br></br>
+                <div className="errorMsgJob">{this.state.errors.level_id}</div> */}
+                <select name="level_id">
+                  <option>
+                      Select Level
+                  </option>
+                  {
+                    levellist.map((item)=>{
+                      return(
+                        <option value={item.id} key={item.id}>{item.level}</option>
+                      )
+                    })
+                  }
+                </select>
+
+                <br></br>
+                <label >Job Role</label>
+                <input  id="job_id" type="number"  name="job_id" onChange={(e)=>this.job_id(e)}></input><br></br>
+                <div className="errorMsgJob">{this.state.errors.job_id}</div>
+
+                <br></br>
+                <label >Resume</label>
+                <input  id="resume" type="file"  name="resume" onChange={(e)=>this.resume(e)}></input><br></br>
+                <div className="errorMsgJob">{this.state.errors.resume}</div>
+
+                <br></br>
+
+                <br></br>
+        {/* </div> */}
+
+                <button id="jobb" className="btn btn-success" onClick={this.addCandidate}>Create</button>
+                <br></br>
+                <br></br>
+                <Link to={'/addcandidate'}><button className="btn btn-outline-dark" type="submit" onClick={()=>{window.location='/addcandidate'}}>Back</button>
+            </Link> 
+                <br></br>
+             
+            </form>
+        </div>
+            
+        )
+     }
+}
+export default Candidate
+
